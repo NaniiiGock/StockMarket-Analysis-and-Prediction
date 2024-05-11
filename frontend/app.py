f4d3c7f8d77412632639c058d2778d290c852cbb
NaniiiGock <liliana.hotsko@ucu.edu.ua>
2024-05-11 12:49:08 +0300

    added streaming

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask import jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import random
-import httpx
 import asyncio
+import httpx
+from concurrent.futures import ThreadPoolExecutor
+from aiokafka import AIOKafkaConsumer
 import json
-from aiokafka import AIOKafkaConsumer, TopicPartition
+
+executor = ThreadPoolExecutor(1)
 
 app = Flask(__name__)
 users = {}
@@ -51,32 +53,99 @@ currency_list = asyncio.run(fetch_tokens())
 #                           KAFKA
 #================================================================================================
 
-async def consume_kafka_messages(topic, kafka_server, partitions):
+# async def consume_kafka_messages(topic, kafka_server, partitions):
+#     consumer = AIOKafkaConsumer(
+#         bootstrap_servers=kafka_server
+#     )
+#     await consumer.start()
+#     try:
+#         topic_partitions = [TopicPartition(topic, p) for p in partitions[:-1]]
+#         consumer.assign(topic_partitions)
+#         async for msg in consumer:
+#             message_value = msg.value.decode('utf-8')
+#             print(f"Received from Kafka (Partition {msg.partition}): {message_value}")
+#             data_point = json.loads(message_value)
+#             plot_name = partition_to_plot.get(msg.partition)
+#             if plot_name and 'value' in data_point:
+#                 data_store[plot_name].append(data_point['value'])
+#     finally:
+#         await consumer.stop()
+
+# def start_kafka_consumer():
+#     kafka_topic = 'prices'
+#     kafka_server = 'stock_api_kafka:9092'
+#     partitions = [0, 1, 2, 3, 4]
+#     loop = asyncio.new_event_loop()
+#     asyncio.set_event_loop(loop)
+#     loop.create_task(consume_kafka_messages(kafka_topic, kafka_server, partitions))
+
+
+# from kafka import KafkaConsumer
+# import json
+
+# # Create a KafkaConsumer
+# consumer = KafkaConsumer(
+#     'your_topic_name',
+#     bootstrap_servers=['localhost:9092'],
+#     auto_offset_reset='earliest',  # Start reading at the earliest message
+#     enable_auto_commit=True,       # Automatically commit the offsets
+#     group_id='my-group',           # Consumer group ID
+#     value_deserializer=lambda x: json.loads(x.decode('utf-8'))  # Deserializer for JSON data
+# )
+
+# # Consume messages
+# for message in consumer:
+#     print(f"Received message: {message.value} on partition {message.partition}")
+
+# # Always close the consumer connection explicitly when done
+# consumer.close()
+
+
+# async def consume():
+#     consumer = AIOKafkaConsumer(
+#         'prices',
+#         bootstrap_servers='stock_api_kafka:9092',
+#         group_id="my-group",
+#         auto_offset_reset='earliest',
+#         session_timeout_ms=30000,
+#         value_deserializer=lambda x: json.loads(x.decode('utf-8')), 
+#         heartbeat_interval_ms=3000
+#     )
+#     await consumer.start()
+#     try:
+#         async for message in consumer:
+#             print(f"Received message: {message.value} on partition {message.partition}")
+#             plot_name = partition_to_plot[message.partition]
+#             data_store[plot_name].append(message.value['value'])
+#             print(data_store[plot_name])
+#     finally:
+#         await consumer.stop()
+        
+async def consume():
+
     consumer = AIOKafkaConsumer(
-        bootstrap_servers=kafka_server
+        'prices',
+        bootstrap_servers='stock_api_kafka:9092',
+        group_id="my-group",
+        auto_offset_reset='earliest',
+        value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
+
     await consumer.start()
     try:
-        topic_partitions = [TopicPartition(topic, p) for p in partitions[:-1]]
-        consumer.assign(topic_partitions)
-        async for msg in consumer:
-            message_value = msg.value.decode('utf-8')
-            print(f"Received from Kafka (Partition {msg.partition}): {message_value}")
-            data_point = json.loads(message_value)
-            plot_name = partition_to_plot.get(msg.partition)
-            if plot_name and 'value' in data_point:
-                data_store[plot_name].append(data_point['value'])
+        async for message in consumer:
+            print(f"Received message: {message.value} on partition {message.partition}")
+            if message.partition in partition_to_plot:
+                data_store[partition_to_plot[message.partition]].append(message.value['value'])
+                print(data_store[partition_to_plot[message.partition]])
     finally:
         await consumer.stop()
 
-def start_kafka_consumer():
-    kafka_topic = 'prices'
-    kafka_server = 'stock_api_kafka:9092'
-    partitions = [0, 1, 2, 3, 4]
+def start_async_tasks():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.create_task(consume_kafka_messages(kafka_topic, kafka_server, partitions))
-
+    loop.run_until_complete(consume())
+    
 #================================================================================================
 #                           ROUTES
 #================================================================================================
@@ -143,5 +212,8 @@ def data_for_plot5():
 ####################################################################################################
 
 if __name__ == '__main__':
-    start_kafka_consumer()
+    # start_kafka_consumer()
+    executor.submit(start_async_tasks)
     app.run(host='0.0.0.0', port=8080, debug=True)
+    # asyncio.run(consume())
+    
