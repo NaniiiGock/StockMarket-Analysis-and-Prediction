a7da7267cc72867fe8fdee20ed4fb325a807663d
Liliana <91615473+NaniiiGock@users.noreply.github.com>
2024-05-12 21:14:58 +0300

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-import random
 import asyncio
 import httpx
 from concurrent.futures import ThreadPoolExecutor
 from aiokafka import AIOKafkaConsumer
 import json
+from flask import Flask, render_template, request, jsonify
+import requests
+import time
 
 executor = ThreadPoolExecutor(1)
 
@@ -19,7 +21,14 @@ data_store = {
     'Bonk': [],
     'W': [],
 }
-
+data_store_times={
+    'HNT': [],
+    'SOL': [],
+    'soLINK': [],
+    'TBTC': [],
+    'Bonk': [],
+    'W': [],
+}
 partition_to_plot = {
     0: 'HNT',
     1: 'SOL',
@@ -50,6 +59,58 @@ async def fetch_tokens(retries=10, delay=5):
 
 currency_list = asyncio.run(fetch_tokens())
 
+#================================================================================================
+#                           SPARK
+#================================================================================================
+
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import from_json, col
+# from flask import Flask, jsonify, render_template
+# import threading
+
+# app = Flask(__name__)
+# data_store = {
+#     'HNT': [],
+#     'SOL': [],
+#     'soLINK': [],
+#     'TBTC': [],
+#     'Bonk': [],
+#     'W': []
+# }
+
+# spark = SparkSession.builder \
+#     .appName("KafkaSparkIntegration") \
+#     .master("local[*]") \
+#     .getOrCreate()
+
+# def consume_from_kafka():
+#     df = spark \
+#         .readStream \
+#         .format("kafka") \
+#         .option("kafka.bootstrap.servers", "localhost:9092") \
+#         .option("subscribe", "prices") \
+#         .option("startingOffsets", "earliest") \
+#         .load()
+
+#     schema = "value STRING, partition INT"  # Adjust schema as per your data
+#     df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
+
+#     query = df \
+#         .writeStream \
+#         .foreachBatch(process_batch) \
+#         .start()
+    
+#     query.awaitTermination()
+
+# def process_batch(df, epoch_id):
+#     if not df.isEmpty():
+#         collected_data = df.collect()
+#         for row in collected_data:
+#             partition_key = row['partition']
+#             value = row['value']
+#             key = list(data_store.keys())[partition_key]  # Convert index to key name
+#             data_store[key].append(value)
+#             data_store[key] = data_store[key][-150:]  # Keep only last 150 entries
 
 #================================================================================================
 #                           KAFKA
@@ -68,10 +129,14 @@ async def consume():
     await consumer.start()
     try:
         async for message in consumer:
-            print(f"Received message: {message.value} on partition {message.partition}")
+            print(f"Received message: {message} on partition {message.partition}")
             if message.partition in partition_to_plot:
                 data_store[partition_to_plot[message.partition]].append(message.value['value'])
-                print(data_store[partition_to_plot[message.partition]])
+                # data_store_times[partition_to_plot[message.partition]].append(message.value['datetime'])
+                if len(data_store[partition_to_plot[message.partition]]) > 150:
+                    data_store[partition_to_plot[message.partition]] = data_store[partition_to_plot[message.partition]][-150:]
+                    # data_store_times[partition_to_plot[message.partition]] = data_store_times[partition_to_plot[message.partition]][-150:]
+                # print(data_store[partition_to_plot[message.partition]])
     finally:
         await consumer.stop()
 
@@ -94,26 +159,71 @@ def login():
         return redirect(url_for('data_selection'))
     return render_template('login.html')
 
-@app.route('/data-selection')
-def data_selection():
-    return render_template('data_selection.html')
-
 @app.route('/history-of-trades')
 def history_of_trades():
     return render_template('history_of_trades.html')
 
 # =================================================================================================
-#                           PLOTS
+#                           Historical data
+# =================================================================================================
+    
+@app.route('/data-selection')
+def data_selection():
+    return render_template('data_selection.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    start_date = request.form['startDateTime'] +":00Z"
+    end_date = request.form['endDateTime'] + ":00Z"
+    currencies = request.form.getlist('currencies')
+    print("=====================================")
+    print("start_date: ", start_date)
+    print("end_date: ", end_date)   
+    print("currencies: ", currencies)
+    print("=====================================")
+    chart_data = parse_data(start_date, end_date, currencies[-1])
+    return jsonify(chart_data)
+
+def parse_data(start_date, end_date, currencies):    
+    api_url = f"http://stock_api:3000/price/interval?id={currencies}&vsToken=USDT&start_time={start_date}&end_time={end_date}"
+    print("API url ", api_url)
+    response = requests.get(api_url)
+
+    print("response:" , response.json())
+    labels = [point['datetime'] for point in response.json()][::-1] 
+    values = [point['value'] for point in response.json()][::-1]
+    return {
+        'labels': labels, 
+        'values': values 
+    }
+
+# =================================================================================================
+#                           Streaming
 # =================================================================================================
 
 
 @app.route('/plot-list')
 def plot_list():
     return render_template('plot_list.html')
+    
+    
 
 @app.route('/data_for_HNT')
 def data_for_HNT():
+    # labels = [x[0] for x in data_store_2['HNT']]
+    # values = [x[1] for x in data_store_2['HNT']]
+    # labels = [list(data.keys())[0] for data in data_store_2['HNT']]
+    # values = [list(data.values())[0] for data in data_store_2['HNT']]
+    # return jsonify({'labels': labels, 'values': values})
     return jsonify({'labels': list(range(len(data_store['HNT']))), 'values': data_store['HNT']})
+
+
+# @app.route('/data_for_SOL')
+# def data_for_SOL():
+#     # Extract labels and values assuming data_store['SOL'] is a list of tuples (datetime, value)
+#     labels = [date_str for date_str in data_store_times['SOL']]
+#     values = [value for _, value in data_store['SOL']]
+#     return jsonify({'labels': labels, 'values': values})
 
 @app.route('/data_for_SOL')
 def data_for_SOL():
@@ -121,18 +231,30 @@ def data_for_SOL():
 
 @app.route('/data_for_soLINK')
 def data_for_soLINK():
+    # labels = [list(data.keys()) for data in data_store_2['soLINK']]
+    # values = [list(data.values()) for data in data_store_2['soLINK']]
+    # return jsonify({'labels': labels, 'values': values})
     return jsonify({'labels': list(range(len(data_store['soLINK']))), 'values': data_store['soLINK']})
 
 @app.route('/data_for_TBTC')
 def data_for_TBTC():
+    # labels = [list(data.keys())[0] for data in data_store_2['TBTC']]
+    # values = [list(data.values())[0] for data in data_store_2['TBTC']]
+    # return jsonify({'labels': labels, 'values': values})
     return jsonify({'labels': list(range(len(data_store['TBTC']))), 'values': data_store['TBTC']})
 
 @app.route('/data_for_Bonk')
 def data_for_Bonk():
+    # labels = [list(data.keys())[0] for data in data_store_2['Bonk']]
+    # values = [list(data.values())[0] for data in data_store_2['Bonk']]
+    # return jsonify({'labels': labels, 'values': values})
     return jsonify({'labels': list(range(len(data_store['Bonk']))), 'values': data_store['Bonk']})
 
 @app.route('/data_for_W')
 def data_for_W():
+    # labels = [list(data.keys())[0] for data in data_store_2['W']]
+    # values = [list(data.values())[0] for data in data_store_2['W']]
+    # return jsonify({'labels': labels, 'values': values})
     return jsonify({'labels': list(range(len(data_store['W']))), 'values': data_store['W']})
 
 ####################################################################################################
@@ -142,3 +264,4 @@ def data_for_W():
 if __name__ == '__main__':
     executor.submit(start_async_tasks)
     app.run(host='0.0.0.0', port=8080)
+
